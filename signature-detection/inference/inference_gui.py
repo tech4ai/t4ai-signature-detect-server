27aaa7fa55e8a801c37e7a1daca50cd7d23aa2fb
samuellimabraz <samuellimabraz@gmail.com>
2024-12-18 12:17:36 -0300

    feat: Inference with request, tritonclient and ultralytics. Gradio interface for test

---
+++ signature-detection/inference/inference_gui.py
@@ -0,0 +1,78 @@
+import os
+import gradio as gr
+from PIL import Image
+import requests
+from inference_pipeline import InferencePipeline, LocalPredictor, VertexAIPredictor
+import numpy as np
+
+# Gradio Interface
+def inference(image, url, use_vertex):
+    if image is None and not url:
+        return "Please upload an image or provide a URL."
+
+    # Handle file input or URL
+    if image is not None and isinstance(image, np.ndarray):
+        print("Image received:", type(image), image.shape)
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        root_path = os.path.join(current_dir, 'tmp')
+        if not os.path.exists(root_path):
+            os.makedirs(root_path, exist_ok=True)
+            
+        image_path = os.path.join(root_path, "temp_image.jpg")
+        try:
+            pil_image = Image.fromarray(image.astype(np.uint8), 'RGB')
+            pil_image.save(image_path, format="JPEG")
+            print(f"Image saved at: {image_path}")
+        except Exception as e:
+            print("Error saving image:", e)
+            return "Failed to save image."
+    else:
+        try:
+            response = requests.get(url, stream=True)
+            if response.status_code == 200:
+                image_path = "temp_image.jpg"
+                with open(image_path, "wb") as f:
+                    f.write(response.content)
+                print("Image downloaded successfully.")
+            else:
+                return "Failed to fetch image from URL."
+        except Exception as e:
+            print("Error fetching image:", e)
+            return "Error downloading image."
+
+    pipeline = InferencePipeline(predictors["local"] if not use_vertex else predictors["vertex"])
+    print(f"Running inference on image: {image_path}")
+
+    try:
+        resp = pipeline.run(image_path)
+    except Exception as e:
+        print("Error during inference:", e)
+        return "Failed to process image."
+
+    # Remove temporary file if created
+    os.remove(image_path)
+
+    return resp['result'].plot()
+
+predictors = {
+    "local":  LocalPredictor(url="http://localhost:8000/v2/models/yolov8s/infer"),
+    "vertex": VertexAIPredictor(url="REDACTED_VERTEX_ENDPOINT")
+}
+
+# Define Gradio Interface
+demo = gr.Interface(
+    fn=inference,
+    inputs=[
+        gr.Image(label="Upload Image"),
+        gr.Textbox(label="Image URL"),
+        gr.Checkbox(label="Use Vertex AI", value=False)
+    ],
+    outputs=gr.Image(label="Inference Result"),
+    title="Image Inference Pipeline",
+    description="Upload an image or enter a URL to perform inference using a Local or Vertex AI Predictor."
+)
+
+demo.launch()
+
+
+
---
+++ signature-detection/inference/inference_pipeline.py
@@ -0,0 +1,152 @@
+import os
+import time
+from typing import Optional
+import subprocess
+
+import gradio as gr
+import requests
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+from utils.postprocessing import postprocess
+from utils.preprocessing import preprocess
+
+## Strategy Pattern for Inference
+class BasePredictor:
+    """Base class for predictors."""
+
+    def __init__(self):
+        pass
+
+    def predict(self, payload):
+        """To be implemented by concrete classes."""
+        raise NotImplementedError("Predict method not implemented.")
+
+class LocalPredictor(BasePredictor):
+    """Inference predictor for local models."""
+
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+
+    def predict(self, payload):
+        tic = time.time()
+        response = requests.post(self.url, headers={"Content-Type": "application/json"}, json=payload)
+        mean_time = time.time() - tic
+        return response.json(), mean_time
+
+class VertexAIPredictor(BasePredictor):
+    """Inference predictor for Vertex AI models."""
+
+    def __init__(self, url, access_token: Optional[str] = None):
+        super().__init__()
+        self.url = url
+        if access_token is None:
+            try:
+                self.access_token = self.get_google_access_token()
+            except Exception as e:
+                print("Error fetching access token:", e)
+                return
+                
+        self.headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json"
+        }
+    
+    def get_google_access_token(self):
+        result = subprocess.run(['gcloud', 'auth', 'print-access-token'], stdout=subprocess.PIPE, text=True)
+        return result.stdout.strip()
+
+    def predict(self, payload):
+        tic = time.time()
+        response = requests.post(self.url, headers=self.headers, json=payload)
+        mean_time = time.time() - tic
+        return response.json(), mean_time
+
+## Inference Pipeline
+class InferencePipeline:
+    """Orchestrates the entire inference pipeline."""
+
+    def __init__(self, predictor: BasePredictor):
+        self.predictor = predictor
+
+    def run(self, image_path):
+        """Run the inference pipeline."""
+        orig_image = cv2.imread(image_path)
+        
+        preprocessed_image = preprocess(image_path)
+        
+        payload = self._create_payload(preprocessed_image)
+
+        response, mean_time = self.predictor.predict(payload)
+
+        tic = time.time()
+        preds = self._process_response(response)
+        result = postprocess(preds[0], preprocessed_image, [orig_image], [image_path])[0]
+        post_mean_time = time.time() - tic
+        
+        # self._visualize_results(results)
+            
+        print(f"Inference time: {mean_time}")
+        return {'result' : result, 'inference_time': mean_time, 'post_process_time': post_mean_time}
+
+    def _create_payload(self, image):
+        """Create the payload for the model."""
+        return {
+            "id": "0",
+            "inputs": [
+                {
+                    "name": "images",
+                    "shape": list(image.shape),
+                    "datatype": "FP32",
+                    "parameters": {},
+                    "data": image.tolist()
+                }
+            ]
+        }
+
+    def _process_response(self, response):
+        """Extract predictions from response."""
+        return [np.array(response['outputs'][0]['data']).astype(np.float32).reshape([1, 5, 8400])]
+
+    def _visualize_results(self, results):
+        """Visualize bounding boxes."""
+        for result in results:
+            im = result.plot()
+            plt.imshow(im)
+            plt.axis('off')
+            plt.show()
+            
+
+## Main Execution
+def main():
+    # Paths
+REDACTED_PATH
+    image_paths = [os.path.join(root_dir_images, img) for img in os.listdir(root_dir_images) if img.endswith(".jpg")]
+
+    # Select predictor: Local or Vertex AI
+    use_vertex_ai = False
+
+    if use_vertex_ai:
+        predictor = VertexAIPredictor(
+            url="REDACTED_VERTEX_ENDPOINT",
+        )
+    else:
+        predictor = LocalPredictor(url="http://localhost:8000/v2/models/yolov8s/infer")
+
+    # Run Pipeline
+    pipeline = InferencePipeline(predictor)
+    i_times = []
+    p_times = []
+    for image_path in image_paths:
+        r= pipeline.run(image_path)
+        i_times.append(r['inference_time'])
+        p_times.append(r['post_process_time'])
+    
+    print(f"Average inference time: {np.mean(i_times)}")
+    print(f"Average post-process time: {np.mean(p_times)}")
+    
+if __name__ == "__main__":
+    main()
+    
