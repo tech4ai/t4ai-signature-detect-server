00fbce1ed555ee7c7ed096aedca4952349be5e30
samuellimabraz <samuellimabraz@gmail.com>
2024-12-31 13:50:01 -0300

    refactor: streamline inference pipeline and update predictor selection mechanism

--- signature-detection/inference/inference_pipeline.py
+++ signature-detection/inference/inference_pipeline.py
@@ -1,71 +1,12 @@
 import os
-import time
-from typing import Optional
-import subprocess
-
-import gradio as gr
-import requests
 import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-import base64
+from pprint import pprint
+from predictors import BasePredictor, HttpPredictor, VertexAIPredictor, TritonClientPredictor
 
 def encode_image(image_path):
     image_data = np.fromfile(image_path, dtype="uint8")
     image_data = np.expand_dims(image_data, axis=0)
     return image_data
-
-## Strategy Pattern for Inference
-class BasePredictor:
-    """Base class for predictors."""
-
-    def __init__(self):
-        pass
-
-    def predict(self, payload):
-        """To be implemented by concrete classes."""
-        raise NotImplementedError("Predict method not implemented.")
-
-class LocalPredictor(BasePredictor):
-    """Inference predictor for local models."""
-
-    def __init__(self, url):
-        super().__init__()
-        self.url = url
-
-    def predict(self, payload):
-        tic = time.time()
-        response = requests.post(self.url, headers={"Content-Type": "application/json"}, json=payload)
-        mean_time = time.time() - tic
-        return response.json(), mean_time
-
-class VertexAIPredictor(BasePredictor):
-    """Inference predictor for Vertex AI models."""
-
-    def __init__(self, url, access_token: Optional[str] = None):
-        super().__init__()
-        self.url = url
-        if access_token is None:
-            try:
-                self.access_token = self.get_google_access_token()
-            except Exception as e:
-                print("Error fetching access token:", e)
-                return
-                
-        self.headers = {
-            "Authorization": f"Bearer {access_token}",
-            "Content-Type": "application/json"
-        }
-    
-    def get_google_access_token(self):
-        result = subprocess.run(['gcloud', 'auth', 'print-access-token'], stdout=subprocess.PIPE, text=True)
-        return result.stdout.strip()
-
-    def predict(self, payload):
-        tic = time.time()
-        response = requests.post(self.url, headers=self.headers, json=payload)
-        mean_time = time.time() - tic
-        return response.json(), mean_time
 
 ## Inference Pipeline
 class InferencePipeline:
@@ -77,66 +18,55 @@ class InferencePipeline:
     def run(self, image_path):
         """Run the inference pipeline."""
         image_data = encode_image(image_path)
-        
-        payload = self._create_payload(image_data)
 
-        response, mean_time = self.predictor.predict(payload)
+        response, mean_time = self.predictor.predict(image_data)
         
         result = self._process_response(response)
-        print(result)
-        
-        print(f"Inference time: {mean_time}")
+   
         return {'result' : result, 'inference_time': mean_time}
-
-    def _create_payload(self, image):
-        """Create the payload for the model."""
-        return {
-            "id": "0",
-            "inputs": [
-                {
-                    "name": "raw_image",
-                    "shape": image.shape,
-                    "datatype": "UINT8",
-                    "data": image.tolist()
-                }
-            ]
-        }
 
     def _process_response(self, response):
         """Extract predictions from response."""
-        data = np.array(response['outputs'][0]['data']).astype(np.float32)
-        # Número de detecções
-        num_detections = data.shape[0] // 5
-        # Reorganize o array em uma matriz de N x 5
-        data = data.reshape((num_detections, 5))
         # Separar bounding boxes e scores
         return {
-            "detection_boxes": data[:, :4],
-            "detection_scores" : data[:, 4],
+            "detection_boxes": response[:, :4],
+            "detection_scores" : response[:, 4],
         }
             
-
 ## Main Execution
 def main():
     # Paths
 REDACTED_PATH
     image_paths = [os.path.join(root_dir_images, img) for img in os.listdir(root_dir_images) if img.endswith(".jpg")]
 
-    # Select predictor: Local or Vertex AI
-    use_vertex_ai = False
+    predictors = {
+        '1': (HttpPredictor, {"url": "REDACTED_TRITON_URL"}),
+        '2': (TritonClientPredictor, {"url": "REDACTED_TRITON_URL"}),
+        '3': (VertexAIPredictor, {"url": "REDACTED_VERTEX_ENDPOINT"}),
+    }
 
-    if use_vertex_ai:
-        predictor = VertexAIPredictor(
-            url="REDACTED_VERTEX_ENDPOINT",
-        )
-    else:
-        predictor = LocalPredictor(url="http://localhost:8000/v2/models/yolov8_ensemble/infer")
+    # Menu for selecting predictor
+    print("Select predictor:")
+    for key, value in predictors.items():
+        print(f"{key}: {value[0].__name__}")
+        
+    selected = input("Enter number: ")
+    predictor = predictors[selected][0](**predictors[selected][1])
 
+    print(f"Using predictor: {predictor.__class__.__name__}")
+    
     # Run Pipeline
     pipeline = InferencePipeline(predictor)
+    
+    # First inference for warm-up
+    print("Running inference on first image...")
+    pipeline.run(image_paths[0])
+    
+    print("Running inference on all images...")
     i_times = []
     for image_path in image_paths:
         r = pipeline.run(image_path)
+        pprint(r)
         i_times.append(r['inference_time'])
     
     print(f"Average inference time: {np.mean(i_times)}")
@@ -144,4 +74,3 @@ REDACTED_PATH
 if __name__ == "__main__":
     main()
     
-# 0.45890190170079775
---
+++ signature-detection/inference/predictors.py
@@ -0,0 +1,189 @@
+from abc import ABC, abstractmethod
+from urllib.parse import urlsplit
+import numpy as np
+from pprint import pprint
+import requests
+import subprocess
+import json
+import time
+from typing import Optional
+
+def encode_image(image_path):
+    image_data = np.fromfile(image_path, dtype="uint8")
+    image_data = np.expand_dims(image_data, axis=0)
+    return image_data
+
+## Strategy Pattern for Inference
+class BasePredictor(ABC):
+    """Base class for predictors."""
+
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def request(self, input: np.ndarray) -> tuple[dict, float]:
+        """To be implemented by concrete classes."""
+        raise NotImplementedError("Request method not implemented.")
+
+    @abstractmethod
+    def format_response(self, response: str) -> np.ndarray:
+        """To be implemented by concrete classes."""
+        raise NotImplementedError("Format response method not implemented.")
+
+    def predict(self, input: np.ndarray) -> tuple[np.ndarray, float]:
+        """To be implemented by concrete classes."""
+        response, mean_time = self.request(input)
+        return self.format_response(response), mean_time
+    
+class HttpPredictor(BasePredictor):
+    """Inference predictor for local models."""
+
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+        self.headers = {"Content-Type": "application/json"}
+
+    def _create_payload(self, image):
+        return {
+            "inputs": [
+                {
+                    "name": "raw_image",
+                    "shape": image.shape,
+                    "datatype": "UINT8",
+                    "data": image.tolist()
+                }
+            ]
+        }
+
+    def request(self, input: np.ndarray) -> tuple[dict, float]:
+        tic = time.time()
+        response = requests.post(self.url, headers=self.headers, data=json.dumps(self._create_payload(input)))
+        latency = time.time() - tic
+        return response.json(), latency
+    
+    def format_response(self, response):
+        out =  np.array(response['outputs'][0]['data']).astype(np.float32)
+        num_detections = out.shape[0] // 5
+        return out.reshape((num_detections, 5))
+
+class VertexAIPredictor(HttpPredictor):
+    """Inference predictor for Vertex AI models."""
+
+    def __init__(self, url, access_token: Optional[str] = None):
+        super().__init__(url=url)
+   
+        if access_token is None:
+            try:
+                self.access_token = self._get_google_access_token()
+            except Exception as e:
+                print("Error fetching access token:", e)
+                return
+                
+        self.headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json"
+        }
+    
+    def _get_google_access_token(self):
+        result = subprocess.run(['gcloud', 'auth', 'print-access-token'], stdout=subprocess.PIPE, text=True)
+        return result.stdout.strip()
+
+class TritonClientPredictor(BasePredictor):
+    """
+    Client for interacting with a remote Triton Inference Server model.
+
+    Attributes:
+        endpoint (str): The name of the model on the Triton server.
+        url (str): The URL of the Triton server.
+        triton_client: The Triton client (either HTTP or gRPC).
+        InferInput: The input class for the Triton client.
+        InferRequestedOutput: The output request class for the Triton client.
+        input_formats (List[str]): The data types of the model inputs.
+        np_input_formats (List[type]): The numpy data types of the model inputs.
+        input_names (List[str]): The names of the model inputs.
+        output_names (List[str]): The names of the model outputs.
+    """
+
+    def __init__(self, url: str, endpoint: str = "", scheme: str = ""):
+        """
+        Initialize the TritonRemoteModel.
+
+        Arguments may be provided individually or parsed from a collective 'url' argument of the form
+            <scheme>://<netloc>/<endpoint>/<task_name>
+
+        Args:
+            url (str): The URL of the Triton server.
+            endpoint (str): The name of the model on the Triton server.
+            scheme (str): The communication scheme ('http' or 'grpc').
+        """
+        if not endpoint and not scheme:  # Parse all args from URL string
+            splits = urlsplit(url)
+            endpoint = splits.path.strip("/").split("/")[0]
+            scheme = splits.scheme
+            url = splits.netloc
+
+        self.endpoint = endpoint
+        self.url = url
+        
+        print(f"Connecting to Triton server at {self.url} with endpoint {self.endpoint} using {scheme}...")
+
+        # Choose the Triton client based on the communication scheme
+        if scheme == "grpc":
+            import tritonclient.grpc as client  # noqa
+
+            self.triton_client = client.InferenceServerClient(url=self.url, verbose=False, ssl=False)
+            config = self.triton_client.get_model_config(endpoint, as_json=True)["config"]
+        else:
+            import tritonclient.http as client  # noqa
+
+            self.triton_client = client.InferenceServerClient(url=self.url, verbose=False, ssl=True)
+            config = self.triton_client.get_model_config(endpoint)
+
+        # Sort output names alphabetically, i.e. 'output0', 'output1', etc.
+        config["output"] = sorted(config["output"], key=lambda x: x.get("name"))
+
+        pprint(self.triton_client.get_inference_statistics(model_name=self.endpoint))
+        # pprint(self.triton_client.get_model_repository_index())
+        
+        # Define model attributes
+        type_map = {"TYPE_FP32": np.float32, "TYPE_FP16": np.float16, "TYPE_UINT8": np.uint8}
+        self.InferRequestedOutput = client.InferRequestedOutput
+        self.InferInput = client.InferInput
+        self.input_formats = [x["data_type"] for x in config["input"]]
+        self.np_input_formats = [type_map[x] for x in self.input_formats]
+        self.input_names = [x["name"] for x in config["input"]]
+        self.output_names = [x["name"] for x in config["output"]]
+        self.output_formats = [x["data_type"] for x in config["output"]]
+        self.np_output_formats = [type_map[x] for x in self.output_formats]
+        self.metadata = eval(config.get("parameters", {}).get("metadata", {}).get("string_value", "None"))
+
+    def request(self, *inputs: np.ndarray) -> tuple[np.ndarray, float]:
+        """
+        Call the model with the given inputs.
+
+        Args:
+            *inputs (List[np.ndarray]): Input data to the model.
+
+        Returns:
+            (List[np.ndarray]): Model outputs.
+        """
+        infer_inputs = []
+        for i, x in enumerate(inputs):
+            if x.dtype != self.np_input_formats[i]:
+                x = x.astype(self.np_input_formats[i])
+            infer_input = self.InferInput(self.input_names[i], [*x.shape], self.input_formats[i].replace("TYPE_", ""))
+            infer_input.set_data_from_numpy(x)
+            infer_inputs.append(infer_input)
+
+        infer_outputs = [self.InferRequestedOutput(output_name) for output_name in self.output_names]
+        
+        tic = time.time()
+        outputs = self.triton_client.infer(model_name=self.endpoint, inputs=infer_inputs, outputs=infer_outputs)
+        latency = time.time() - tic
+
+        return outputs.as_numpy(self.output_names[0]).astype(self.np_output_formats[0]), latency
+    
+    def format_response(self, response):
+        num_detections = response.shape[0]
+        return response.reshape((num_detections, 5))
+ 
