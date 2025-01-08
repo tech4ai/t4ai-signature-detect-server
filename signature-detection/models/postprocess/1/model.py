402512768d96adab640e2596348ef93f278056c3
samuellimabraz <samuellimabraz@gmail.com>
2025-01-08 17:42:45 -0300

    refactor: enhance model preprocessing class with improved documentation and code organization

--- signature-detection/models/postprocess/1/model.py
+++ signature-detection/models/postprocess/1/model.py
@@ -1,128 +1,135 @@
-import numpy as np
 import json
-import triton_python_backend_utils as pb_utils
-import cv2
-import time
 import logging
+import time
+
+import cv2
+import numpy as np
+import triton_python_backend_utils as pb_utils
 
 
 class TritonPythonModel:
-    """Your Python model must use the same class name. Every Python model
-    that is created must have "TritonPythonModel" as the class name.
+    """
+    A Python model class for Triton Inference Server.
+    This class must be named `TritonPythonModel` and implements the methods
+    required to initialize, execute, and finalize the model.
     """
 
     def initialize(self, args):
-        """`initialize` is called only once when the model is being loaded.
-        Implementing `initialize` function is optional. This function allows
-        the model to intialize any state associated with this model.
+        """
+        Initializes the model when it is being loaded.
+        This method is called once during the model's lifetime and is used to set up any
+        necessary configurations or state required for inference.
+
         Parameters
         ----------
         args : dict
-          Both keys and values are strings. The dictionary keys and values are:
-          * model_config: A JSON string containing the model configuration
-          * model_instance_kind: A string containing model instance kind
-          * model_instance_device_id: A string containing model instance device ID
-          * model_repository: Model repository path
-          * model_version: Model version
-          * model_name: Model name
+            A dictionary containing initialization arguments, with keys and values as strings:
+            - model_config: JSON string containing the model configuration.
+            - model_instance_kind: String indicating the model instance kind.
+            - model_instance_device_id: String indicating the model instance device ID.
+            - model_repository: Path to the model repository.
+            - model_version: Version of the model.
+            - model_name: Name of the model.
         """
-
-        # You must parse model_config. JSON string is not parsed here
+        # Parse the model configuration from JSON
         self.model_config = model_config = json.loads(args['model_config'])
 
-        # Get OUTPUT0 configuration
+        # Retrieve the output configuration for "detection_result"
         detection_result_config = pb_utils.get_output_config_by_name(
             model_config, "detection_result")
 
-        # Convert Triton types to numpy types
+        # Convert Triton data types to NumPy data types
         self.detection_result_dtype = pb_utils.triton_string_to_numpy(
             detection_result_config['data_type'])
 
-        self.score_threshold = 0.1
-        self.nms_threshold = 0.45
+        # Set thresholds for score and Non-Maximum Suppression (NMS)
+        self.score_threshold = 0.1  # Minimum confidence score for a detection to be valid
+        self.nms_threshold = 0.45  # NMS overlap threshold
 
     def execute(self, requests):
-        """`execute` MUST be implemented in every Python model. `execute`
-        function receives a list of pb_utils.InferenceRequest as the only
-        argument. This function is called when an inference request is made
-        for this model. Depending on the batching configuration (e.g. Dynamic
-        Batching) used, `requests` may contain multiple requests. Every
-        Python model, must create one pb_utils.InferenceResponse for every
-        pb_utils.InferenceRequest in `requests`. If there is an error, you can
-        set the error argument when creating a pb_utils.InferenceResponse
+        """
+        Handles inference requests and returns the corresponding responses.
+
+        This method processes one or more requests, performs post-processing
+        (Filtering and Non-Maximum Suppression), and constructs responses.
+
         Parameters
         ----------
         requests : list
-          A list of pb_utils.InferenceRequest
+            A list of `pb_utils.InferenceRequest` objects containing input tensors for inference.
+
         Returns
         -------
         list
-          A list of pb_utils.InferenceResponse. The length of this list must
-          be the same as `requests`
+            A list of `pb_utils.InferenceResponse` objects, one for each request.
         """
         responses = []
         times = []
-        # Every Python backend must iterate over everyone of the requests
-        # and create a pb_utils.InferenceResponse for each of them.
+
         for request in requests:
             tic = time.time_ns()
-            # Get INPUT0
+
+            # Retrieve the input tensor "postprocess_input"
             in_0 = pb_utils.get_input_tensor_by_name(request, "postprocess_input")
 
-            # Get the output arrays from the results
+            # Extract NumPy array from the input tensor
             outputs = in_0.as_numpy()
 
+            # Transpose and process the input tensor
             outputs = np.array([cv2.transpose(outputs[0])])
             rows = outputs.shape[1]
 
             boxes = []
             scores = []
+
+            # Process each row to extract bounding boxes and scores
             for i in range(rows):
                 classes_scores = outputs[0][i][4:]
-                (minScore, maxScore, minClassLoc, (x, maxClassIndex)
-                 ) = cv2.minMaxLoc(classes_scores)
+                _, maxScore, _, (x, maxClassIndex) = cv2.minMaxLoc(classes_scores)
+
                 if maxScore >= self.score_threshold:
-                    box = [outputs[0][i][0] -
-                           (0.5 *
-                            outputs[0][i][2]), outputs[0][i][1] -
-                           (0.5 *
-                            outputs[0][i][3]), outputs[0][i][2], outputs[0][i][3]]
+                    # Compute bounding box coordinates and add them to the list
+                    box = [
+                        outputs[0][i][0] - (0.5 * outputs[0][i][2]),
+                        outputs[0][i][1] - (0.5 * outputs[0][i][3]),
+                        outputs[0][i][2],
+                        outputs[0][i][3]
+                    ]
                     boxes.append(box)
                     scores.append(maxScore)
 
-            result_boxes = cv2.dnn.NMSBoxes(boxes, scores,
-                                            self.score_threshold,
-                                            self.nms_threshold,
-                                            0.5)
+            # Apply Non-Maximum Suppression to filter overlapping boxes
+            result_boxes = cv2.dnn.NMSBoxes(
+                boxes, scores, self.score_threshold, self.nms_threshold, 0.5)
 
-
+            # Construct output boxes with scores
             output_boxes_with_scores = []
             for i in range(len(result_boxes)):
                 index = result_boxes[i]
-                # [x_min, y_min, width, height, score]
-                box_with_score = boxes[index] + [scores[index]]
+                box_with_score = boxes[index] + [scores[index]]  # [x_min, y_min, width, height, score]
                 output_boxes_with_scores.append(box_with_score)
-                
+
             times.append(time.time_ns() - tic)
 
+            # Convert the result to a Triton tensor and prepare the response
             detection_boxes_scores = np.array(output_boxes_with_scores)
-            detection_boxes_scores = pb_utils.Tensor(
+            detection_boxes_scores_tensor = pb_utils.Tensor(
                 "detection_result", detection_boxes_scores.astype(self.detection_result_dtype))
 
             inference_response = pb_utils.InferenceResponse(
-                output_tensors=[
-                    detection_boxes_scores,
-                ])
-            
+                output_tensors=[detection_boxes_scores_tensor])
+
             responses.append(inference_response)
 
-        logging.info(f"Average [Postprocess] time: {np.mean(times)}ns")
-        
+        # Log the average processing time
+        logging.info(f"Average [Postprocess] time: {np.mean(times)} ns")
+
         return responses
 
     def finalize(self):
-        """`finalize` is called only once when the model is being unloaded.
-        Implementing `finalize` function is OPTIONAL. This function allows
-        the model to perform any necessary clean ups before exit.
+        """
+        Finalizes the model when it is being unloaded.
+        This method is called once during the model's lifetime and can be used to
+        clean up resources or perform other teardown tasks.
         """
         pass
--- signature-detection/models/preprocess/1/model.py
+++ signature-detection/models/preprocess/1/model.py
@@ -1,100 +1,108 @@
-import numpy as np
 import json
-import triton_python_backend_utils as pb_utils
-import cv2
-import time
 import logging
+import time
+
+import cv2
+import numpy as np
+import triton_python_backend_utils as pb_utils
 
 
 class TritonPythonModel:
-    """Your Python model must use the same class name. Every Python model
-    that is created must have "TritonPythonModel" as the class name.
+    """
+    A Python model class for preprocessing input data for Triton Inference Server.
+    This class must be named `TritonPythonModel` and implements the methods required to
+    initialize, execute, and finalize the model.
     """
 
     def initialize(self, args):
-        """`initialize` is called only once when the model is being loaded.
-        Implementing `initialize` function is optional. This function allows
-        the model to intialize any state associated with this model.
+        """
+        Initializes the model when it is being loaded.
+        This method is called once during the model's lifetime and is used to set up any
+        necessary configurations or state required for preprocessing.
+
         Parameters
         ----------
         args : dict
-          Both keys and values are strings. The dictionary keys and values are:
-          * model_config: A JSON string containing the model configuration
-          * model_instance_kind: A string containing model instance kind
-          * model_instance_device_id: A string containing model instance device ID
-          * model_repository: Model repository path
-          * model_version: Model version
-          * model_name: Model name
+            A dictionary containing initialization arguments, with keys and values as strings:
+            - model_config: JSON string containing the model configuration.
+            - model_instance_kind: String indicating the model instance kind.
+            - model_instance_device_id: String indicating the model instance device ID.
+            - model_repository: Path to the model repository.
+            - model_version: Version of the model.
+            - model_name: Name of the model.
         """
-
-        # You must parse model_config. JSON string is not parsed here
+        # Parse the model configuration from JSON
         self.model_config = model_config = json.loads(args['model_config'])
 
-        # Get OUTPUT0 configuration
+        # Retrieve the output configuration for "preprocessed_image"
         preprocessed_image_config = pb_utils.get_output_config_by_name(
             model_config, "preprocessed_image"
         )
 
-        # Convert Triton types to numpy types
+        # Convert Triton data types to NumPy data types
         self.preprocessed_image_dtype = pb_utils.triton_string_to_numpy(
             preprocessed_image_config['data_type']
         )
 
     def execute(self, requests):
-        """`execute` MUST be implemented in every Python model. `execute`
-        function receives a list of pb_utils.InferenceRequest as the only
-        argument. This function is called when an inference request is made
-        for this model. Depending on the batching configuration (e.g. Dynamic
-        Batching) used, `requests` may contain multiple requests. Every
-        Python model, must create one pb_utils.InferenceResponse for every
-        pb_utils.InferenceRequest in `requests`. If there is an error, you can
-        set the error argument when creating a pb_utils.InferenceResponse
+        """
+        Handles inference requests and returns preprocessed data.
+
+        This method processes one or more requests, decodes and preprocesses images,
+        and constructs responses with the preprocessed tensors.
+
         Parameters
         ----------
         requests : list
-          A list of pb_utils.InferenceRequest
+            A list of `pb_utils.InferenceRequest` objects containing input tensors for preprocessing.
+
         Returns
         -------
         list
-          A list of pb_utils.InferenceResponse. The length of this list must
-          be the same as `requests`
+            A list of `pb_utils.InferenceResponse` objects, one for each request.
         """
         responses = []
         times = []
-        # Every Python backend must iterate over everyone of the requests
-        # and create a pb_utils.InferenceResponse for each of them.
+
         for request in requests:
-            # Get INPUT0
+            # Retrieve the input tensor "raw_image"
             raw_image = pb_utils.get_input_tensor_by_name(request, "raw_image")
 
             tic = time.time_ns()
+
+            # Decode and preprocess the input image
             img = raw_image.as_numpy()
             image = cv2.imdecode(np.frombuffer(img.tobytes(), np.uint8), cv2.IMREAD_COLOR)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            image = cv2.resize(image, (640, 640))
-            image = image.astype(np.float32) / 255.0
-            image = np.transpose(image, (2, 0, 1))  # [H, W, C] -> [C, H, W]
-            image = np.expand_dims(image, axis=0)  # Batch dimension
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert to RGB
+            image = cv2.resize(image, (640, 640))          # Resize to 640x640
+            image = image.astype(np.float32) / 255.0       # Normalize pixel values to [0, 1]
+            image = np.transpose(image, (2, 0, 1))         # Rearrange dimensions [H, W, C] -> [C, H, W]
+            image = np.expand_dims(image, axis=0)          # Add batch dimension
+
             times.append(time.time_ns() - tic)
 
+            # Convert the preprocessed image to a Triton tensor
             preprocessed_image_tensor = pb_utils.Tensor(
                 'preprocessed_image',
                 image.astype(self.preprocessed_image_dtype),
             )
 
+            # Create an inference response
             inference_response = pb_utils.InferenceResponse(
                 output_tensors=[preprocessed_image_tensor]
             )
-            
+
             responses.append(inference_response)
 
-        logging.info(f"Average [Pre-process] time: {np.mean(times)}ns")
-        
+        # Log the average preprocessing time
+        logging.info(f"Average [Pre-process] time: {np.mean(times)} ns")
+
         return responses
 
     def finalize(self):
-        """`finalize` is called only once when the model is being unloaded.
-        Implementing `finalize` function is OPTIONAL. This function allows
-        the model to perform any necessary clean ups before exit.
+        """
+        Finalizes the model when it is being unloaded.
+        This method is called once during the model's lifetime and can be used to
+        clean up resources or perform other teardown tasks.
         """
         pass
