b3a7b4c2aab1741143414a3bf5b209c8957b908a
samuellimabraz <samuellimabraz@gmail.com>
2025-01-10 15:34:02 -0300

    feat: load environment variables from .env file in download_roboflow.py

--- signature-detection/data/download_roboflow.py
+++ signature-detection/data/download_roboflow.py
@@ -1,13 +1,15 @@
 import os
+from dotenv import load_dotenv
 
 from roboflow import Roboflow
 
 HOME = os.getcwd()
 DATASET_DIR = f"{HOME}/signature-detection/data/datasets"
 
+
 def setup_roboflow(n_version=3):
     """Initialize Roboflow and create necessary directories"""
-    os.makedirs(f"{HOME}/datasets/", exist_ok=True)
+    os.makedirs(DATASET_DIR, exist_ok=True)
 
     if "ROBOFLOW_API_KEY" not in os.environ:
         raise ValueError("ROBOFLOW_API_KEY environment variable not set")
@@ -17,6 +19,7 @@ def setup_roboflow(n_version=3):
     project = workspace.project("signature-detection-hlx8j")
     version = project.version(n_version)
     return project, version
+
 
 def download_dataset(version, format: str = "yolov8"):
     """
@@ -28,17 +31,22 @@ def download_dataset(version, format: str = "yolov8"):
         dataset object and its location
     """
     dataset = version.download(
-        model_format=format,
-        location=DATASET_DIR,
-        overwrite=True
+        model_format=format, location=DATASET_DIR, overwrite=True
     )
 
     print("Dataset location:", dataset.location)
     return dataset
 
+
 def main():
+    dotenv_path = os.path.abspath(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", ".env")
+    )
+    load_dotenv(dotenv_path=dotenv_path, verbose=True, override=True)
+
     project, version = setup_roboflow()
     download_dataset(version)
-    
+
+
 if __name__ == "__main__":
     main()
